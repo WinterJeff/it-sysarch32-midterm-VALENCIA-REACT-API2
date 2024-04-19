31bd5fba091e0ac7b361fa6599a4ce03df473ec1
WinterJeff <jpaulcv1@gmail.com>
2024-04-19 22:08:01 +0800

    ep11

---
+++ node-jeff-codex/api/models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    email: { type: String, 
+        required: true, 
+        unique: true,
+        match: /(?!^[.+&'_-]*@.*$)(^[_\w\d+&'-]+(\.[_\w\d+&'-]*)*@[\w\d-]+(\.[\w\d-]+)*\.(([\d]{1,3})|([\w]{2,}))$)/
+    },
+    password: { type: String, required: true }
+});
+
+module.exports = mongoose.model('User', userSchema);
